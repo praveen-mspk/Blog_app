725f7cae00a057c97f248ca8fada1f479d1000b1
Mousigan Iyyamperumal <mousiganofficial@gmail.com>
2026-04-20 03:30:08 -0700

    security dto's added

---
+++ backend/src/main/java/com/bloghub/dto/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package com.bloghub.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+    private Long userId;
+    private String name;
+    private String email;
+}
